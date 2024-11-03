19f06d52efde32309a47807d9d5c48857b6dddaa
syphxncodes <f20230254@dubai.bits-pilani.ac.in>
2024-11-03 23:38:13 +0400

    file added

---
+++ Tile.java
@@ -0,0 +1,15 @@
+public abstract class Tile {
+
+
+    int tileCoordinate;
+    Tile(int tileCoordinate){
+        this.tileCoordinate=tileCoordinate;
+    }
+    public abstract boolean isTileOccupied();
+    public abstract Piece getPiece();
+    public static final class EmptyTile{
+        EmptyTile(int coordinate){
+
+        }
+    }
+}
